fdc5b23b93c15ad61388f0ea3d3932f485e14da7
julian69 <saudadeh@gmail.com>
2015-03-10 18:10:22 -0300

    comments modal window
    ventana modal sin data

--- application/views/profile/commentsview.php
+++ application/views/profile/commentsview.php
@@ -2,49 +2,69 @@
 <section class="col-md-9" id="profile-mid-col">
 	
 	<h1 class="comments-section">Comments</h1>
+	<a href="#" class="btn btn-default btn-block btn-profile" id="add-fav" data-toggle="modal" data-target="#comments_modal">DEJAR UN COMENTARIO  <span class="glyphicon glyphicon-comment" aria-hidden="true"></span></a>
 
 	<?php 
 		for ($i=0; $i < sizeof($comments); $i++) { 
 	?>	
-
-	<article class="col-md-12 pic-sq-grid">
-		<ul class="owner-container clearfix">
-			
-			<li class="owner-pic">
-				<a href="#">
-					<img src="<?php echo $comments[$i]['photo_path']; ?>" alt="">
-				</a>
-			</li>
-
-			<li class="owner-detail">
-				<dl>
-					<dt>
-						<h2><?php echo $comments[$i]['name'].' '.$comments[$i]['last_name']; ?></h2>
-					</dt>
+			<article class="col-md-12 pic-sq-grid">
+				<ul class="owner-container clearfix">
 					
-					<dd>
-						<span>
-							<?php 
+					<li class="owner-pic">
+						<a href="#">
+							<img src="<?php echo $comments[$i]['photo_path']; ?>" alt="">
+						</a>
+					</li>
 
-								$comment_date = strtotime($comments[$i]['date']);
-								$comment_date = date('l jS \of F Y h:i:s A', $comment_date); 
-								echo $comment_date;
-							?>
-						</span>
-					</dd>
-				</dl>
-			</li>
+					<li class="owner-detail">
+						<dl>
+							<dt>
+								<h2><?php echo $comments[$i]['name'].' '.$comments[$i]['last_name']; ?></h2>
+							</dt>
+							
+							<dd>
+								<span>
+									<?php 
 
-			<li class="owner-comment">
-				<span><?php echo $comments[$i]['comment']; ?></span>
-			</li>
+										$comment_date = strtotime($comments[$i]['date']);
+										$comment_date = date('l jS \of F Y h:i:s A', $comment_date); 
+										echo $comment_date;
+									?>
+								</span>
+							</dd>
+						</dl>
+					</li>
 
-		</ul>
-	</article>
+					<li class="owner-comment">
+						<span><?php echo $comments[$i]['comment']; ?></span>
+					</li>
 
- 	<?php 
+				</ul>
+			</article>
+	<?php 
      	}// end for
 	?>
 
 </section>
 
+<!-- comments_modal -->
+<div class="modal fade" id="comments_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+     
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">DEJA TU COMENTARIO  <span class="glyphicon glyphicon-comment" aria-hidden="true"></span></h4>
+      </div>
+
+      <div class="modal-body">
+        <textarea></textarea>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default">ENVIAR</button>
+      </div>
+
+    </div>
+  </div>
+</div>
