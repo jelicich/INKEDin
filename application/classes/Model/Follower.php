be8e893ec6a213ae3c2d392dc66d0b7a4b686df9
julian69 <saudadeh@gmail.com>
2015-03-11 17:55:37 -0300

    get followers

---
+++ application/classes/Model/Follower.php
@@ -0,0 +1,25 @@
+<?php
+
+class Model_Follower extends ORM{
+
+	public function get_followers_by_profile($profile_id)
+	{
+		$followers = $this->select('follower.follower_id', 'follower.date', 'users.name', 'users.last_name', 'users.id','users.photo_id','photos.photo')
+			->where('follower.user_id','=', $profile_id)
+			->join('users')
+			->on('follower.follower_id','=','users.id')
+			->join('photos', 'LEFT')
+			->on('users.photo_id','=','photos.id')
+			->order_by('follower.date', 'DESC')
+			->find_all();
+
+		$followers = $followers->as_array();
+
+		for($i = 0; $i < sizeof($followers); $i++)
+		{
+			$followers[$i] = $followers[$i]->as_array();
+		}
+
+		return $followers;	
+	}
+}
