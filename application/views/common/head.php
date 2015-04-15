ab46053e8cca7556422685c6b3ef0bc7ab54e1a2
julian69 <saudadeh@gmail.com>
2015-04-14 22:17:08 -0300

    metas (scale)

--- application/views/common/head.php
+++ application/views/common/head.php
@@ -1,5 +1,7 @@
 <title><?php echo $title ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="DESCRIPTION" content="">
 <meta name="KEYWORDS" content="">
 <?php echo $common_scripts; echo $common_styles; ?>
