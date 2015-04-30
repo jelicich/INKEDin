e45d23d4f78917713477cfd0e8408bf2e1e71c93
mistlav <saudadeh@gmail.com>
2015-05-01 03:46:04 +0800

    terminos

--- application/views/terms/termsview.php
+++ application/views/terms/termsview.php
@@ -22,30 +22,67 @@
 			<!-- left col -->
 			<div class="col-md-12">
 				<h2>Terminos y condiciones</h2>
-			</div>
-			<div class="col-md-6">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed accumsan mi, id aliquam neque. Nunc aliquam metus vitae orci faucibus congue. Aenean at metus nec dui blandit pulvinar. Aliquam in nisi eget odio fringilla aliquam sit amet sed nisl. Curabitur dignissim massa nec leo varius, nec auctor ligula bibendum. Donec interdum pharetra ornare. Sed vulputate erat enim, non rhoncus ex scelerisque ut. In hac habitasse platea dictumst. Sed dignissim mauris nec arcu dignissim, vitae blandit urna mattis. Proin est metus, pellentesque eget auctor non, fermentum vel ex. Quisque elementum id elit porttitor condimentum. Nullam imperdiet aliquet purus, quis tempus nibh finibus nec. Maecenas dapibus neque eget magna finibus sodales. Etiam semper libero id sem aliquam, in feugiat arcu placerat. Sed in nunc at nulla consectetur fringilla et a orci.</p>
+			
+				<p>Aceptacion de uso, terminos y condiciones de www.Inkedin.com
+					Su acceso y uso de www.Inkedin.com esta sujeto exclusivamente a estos terminos y condiciones. Usted no utilizlara el sitio para ningun proposito 
+					que sea ilegal o prohibido por estos terminos y condicions. Al utililzar usted esta acemptando completamente los terminos, condiciones y descargas 
+					contenidas en este documento. Si usted no acepta estos terminos y condiciones usted debe inemdiatamente dejar de utilizar el mismo.
+				</p>
 
-				<p>Donec scelerisque vel nisl sed accumsan. Nullam eros mi, tempus volutpat justo sed, dictum mattis nibh. Etiam rutrum imperdiet purus, nec volutpat erat egestas sed. Donec sed dapibus tortor, at tincidunt arcu. Aliquam vel tellus ac diam pulvinar sollicitudin id ac tellus. Quisque hendrerit velit non sapien porttitor, nec eleifend tellus tincidunt. Nulla at dictum quam, at scelerisque velit. Mauris auctor arcu velit, ac rhoncus massa mollis at. Maecenas semper consectetur elementum. Nulla facilisi. Proin volutpat ultricies tortor, nec fringilla felis faucibus consectetur. Aenean a dolor non urna fringilla condimentum. Sed mauris tellus, cursus nec tempus et, accumsan id dui.</p>
+				<h2>Detalle tarjeta de credito</h2>
+				<p>www.Inkedin.com nunca solicitara detalles sobre tarjetas de credito y solicita a su vez que no ingrese estos datos en ninguno de los formularios existentes 
+					en www.Inkedin.com.
+				</p>
 
-				<p>Nunc ornare dolor nec feugiat pretium. Donec vitae viverra felis. Aliquam id facilisis nulla, consequat mollis augue. Donec congue, est at faucibus vehicula, risus risus malesuada lacus, eget convallis tortor elit id tellus. Etiam vehicula, nulla nec hendrerit porttitor, nunc mauris dignissim leo, vel aliquet mauris mauris vitae lectus. Integer lacinia ex enim, a laoreet eros ornare ac. Phasellus vel justo velit. Aliquam facilisis, magna a feugiat efficitur, nisi urna ornare libero, vestibulum elementum risus elit id ex. Sed eget mi eu lectus gravida rutrum. In feugiat nec arcu quis rutrum.</p>
+				<h2>Asesoramiento</h2>
+				<p>Los contenidos de www.Inkedin.com no constituyen asesotamiento y no deberia ser tenido en cuenta para tomar o dejar de tomar cualquier tipo de desicion.</p>
 
-				<p>Fusce pharetra bibendum elit eget pellentesque. Fusce aliquet diam urna, a auctor quam dictum at. Vivamus libero tortor, tristique ut orci at, eleifend mollis nisi. Nullam sit amet risus eu ex varius aliquam. Nunc nec tellus nec magna semper accumsan. Donec vitae fringilla diam, in dignissim augue. Maecenas euismod viverra dui nec rhoncus. Suspendisse potenti.</p>
 
-				<p>Aenean massa nunc, sollicitudin fermentum magna sit amet, semper ultrices massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sodales lacinia imperdiet. Vivamus purus mi, lacinia quis fringilla eget, vestibulum ac turpis. Morbi lorem erat, faucibus a magna id, porta rhoncus mi. Vivamus orci massa, blandit ut felis non, posuere ultricies sem. Etiam vehicula venenatis velit, nec iaculis nibh convallis at. Cras volutpat urna vel odio faucibus sagittis. Morbi ipsum dui, rutrum et ex et, viverra interdum mi. Sed accumsan ligula sit amet sapien fermentum tincidunt. Mauris lacinia imperdiet orci ut congue. Donec rutrum, erat vel suscipit commodo, mauris est condimentum lectus, sit amet euismod magna mauris at risus.</p>				
-			</div>
-			<!-- end left col -->
-			<div class="col-md-6">
-				<p>Praesent enim dui, sollicitudin non libero eget, cursus ullamcorper nisi. Mauris et finibus leo. Etiam gravida ante non convallis tristique. Fusce dapibus ipsum dapibus, mollis sem eu, placerat augue. Ut elementum congue libero in imperdiet. Aenean a cursus dui, ut dapibus est. Quisque tempor nulla in lacus cursus pharetra. Nullam porttitor, sem vitae ornare dictum, nisl dui facilisis eros, ac facilisis ipsum tellus ut nunc. Vestibulum vulputate posuere dictum. Proin cursus eget urna quis sodales. Vestibulum dictum ipsum leo, in condimentum justo dignissim et. Mauris feugiat mauris nec iaculis dictum. Integer blandit est in mi cursus, non mollis odio aliquet. Morbi augue nulla, luctus vel ullamcorper sed, lacinia et eros. Proin quis dui vitae quam rhoncus tempor.</p>
+				<h2>Cambio de uso</h2>
+				<p>
+				    www.Inkedin.com se reserva el derecho de:<br>
+					<b>4.1</b> Cambiar o remover (temporal o permanentemente) el sitio o cualquier parte del mismo sin aviso y usted confirma que www.Inkedin.com no sera responsable ante usted por ninguno de estos cambios o eliminacion y
+					<br>
+					<b>4.2</b> Cambiar estos terminos y condiciones en cualquier momento, y se considerará como aceptacion de tales cambios el uso continuado del sitio web después de dichas modificaciones.  
+				</p>
 
-				<p>Vivamus molestie ultricies massa, sit amet mattis magna sodales aliquam. Integer blandit nibh congue placerat tristique. Pellentesque nec pharetra est, eu pulvinar dui. Aliquam eu odio et elit bibendum mattis. Phasellus posuere molestie erat, vel molestie dui sollicitudin ac. Nulla cursus erat a ex mattis egestas. Sed at malesuada urna. Fusce sed lacus sed justo placerat euismod a non ex. Ut et ante ac sapien tincidunt bibendum et nec turpis. Suspendisse egestas suscipit vulputate. Sed massa urna, fermentum id tincidunt eget, hendrerit quis odio. Sed quam felis, interdum et enim lobortis, dignissim luctus lectus. Vivamus vulputate eros quis nulla accumsan, vitae sollicitudin nulla ullamcorper.</p>
 
-				<p>Praesent sodales congue tellus eu varius. Donec scelerisque augue non augue facilisis, quis congue justo malesuada. Vestibulum laoreet sollicitudin enim nec mollis. Etiam vel lacinia ex. Nunc sem sem, aliquet ac lectus at, aliquam iaculis orci. Integer sed odio et urna interdum aliquam at vitae leo. Curabitur velit enim, maximus egestas magna volutpat, pharetra commodo sapien. Donec condimentum vitae elit ac faucibus.</p>
+				<h2>Enlaces a sitios web de terceros</h2>
+				<p>www.Inkedin.com podria incluir links a sitios web de terceros que son controlados y mantenidos por otros. Ningun link a otro sitio es una aprovacion de tales sitios web y usted reconoce y acepta que no somos responsables por el contenido o la disponibilidad de dichos sitios.</p>
 
-				<p>Nam mi leo, consequat in interdum nec, elementum non odio. Nulla ac porta nulla, id porta felis. Suspendisse at bibendum lorem. Pellentesque id nisi ipsum. Aliquam lobortis, ipsum in tincidunt varius, tortor lacus lacinia enim, nec placerat ipsum leo quis diam. Nam libero nunc, fermentum ac nulla eu, elementum commodo massa. Maecenas ac quam at quam elementum faucibus eget sit amet tortor. Aliquam gravida, purus eu suscipit pretium, odio tellus posuere dui, sit amet tincidunt eros odio at mauris. Aliquam finibus ligula at felis porta, vel venenatis nisl consectetur. Nullam et sapien mollis, ultricies nisl at, semper sapien. Fusce eget justo elit. Integer et vestibulum nisl. Integer in tellus faucibus, rutrum lorem at, posuere mi. Nunc elementum placerat justo ac semper. Nulla tempor lorem eu leo venenatis, ac tristique eros vestibulum. Integer accumsan posuere nulla, vitae tempus elit placerat id.</p>
 
-				<p>Pellentesque venenatis eget mi ac consequat. Aliquam at imperdiet nisi. Nam nec arcu congue, sagittis tortor in, placerat libero. Sed sollicitudin lacus nulla, sed maximus nunc facilisis in. Ut fermentum, leo dictum interdum venenatis, tortor justo euismod dui, ac pellentesque enim urna in lorem. Vivamus sagittis quam et fringilla consectetur. Sed laoreet ultrices lectus, id elementum turpis suscipit at. Proin quis nunc malesuada leo accumsan rutrum eu molestie quam. Integer ornare lectus vitae ultrices feugiat. Donec tincidunt sem semper fermentum dignissim. Duis mi purus, pharetra quis venenatis quis, aliquam vel magna. Ut rutrum dapibus auctor. Sed a nisl cursus, viverra urna quis, fringilla velit. Fusce hendrerit vestibulum sem, et placerat diam pellentesque id. Ut aliquet, tortor dignissim laoreet bibendum, enim sem vulputate augue, nec mattis nulla neque a mauris.</p>
-			</div>	
+ 				<h2>Copyright</h2>
+				<p>
+					<b>6.1</b> Todos los derechos de autor, marcas comerciales y otros derechos de propiedad intelectual en el sitio web y su contenido (incluyendo sin limitación el diseño de sitios web, texto, gráficos y todos los software y códigos fuente relacionados con el sitio web) son propiedad de o licenciadas a www.Inkedin.com o de otra forma utilizado por www.Inkedin.com segun lo permitido por la ley.
+					<br>
+					<b>6.2</b> Al acceder al sitio Web, usted acepta que accede a los contenidos únicamente para su uso personal, no comercial. Ninguna parte del contenido podrá ser descargado, copiado, reproducido, transmitido, almacenado, vendido o distribuido sin el consentimiento previo por escrito del titular de los derechos de autor. Esto excluye la descarga, copia y / o impresión de páginas de la Web sólo para uso personal, no comercial.
+				</p>
+
+
+
+				<h2>Aviso legal y limitación de responsabilidad</h2>
+				<p>
+					<b>7.1</b> El sitio web se proporciona sobre una base TAL CUAL y SEGÚN DISPONIBILIDAD sin ninguna representación o aval y sin garantía de ningún tipo, ya sea expresa o implícita, incluyendo pero no limitado a las garantías implícitas de calidad satisfactoria, idoneidad para un propósito particular, no infracción, compatibilidad, seguridad y exactitud.
+					<br>
+					<b>7.2 </b> En la medida permitida por la ley, www.Inkedin.com no será responsable por cualquier pérdida o daño indirecto o consecuente que sea (incluyendo sin limitación la pérdida de negocio, oportunidad, datos, ganancias) que surjan de o en conexión con el uso de el sitio web.
+					<br>
+					<b>7.3 </b> www.Inkedin.com no garantiza que la funcionalidad del sitio web sea ininterrumpido o libre de errores, que los defectos serán corregidos o que el sitio web o el servidor que lo pone a disposición estén libres de virus o cualquier otra cosa que puede ser dañino o destructivo .
+
+					<br>
+					<b>7.4 </b> Nada en estos Términos y Condiciones se interpretará en el sentido de excluir o limitar la responsabilidad de www.Inkedin.com por muerte o lesiones personales como resultado de la negligencia de www.Inkedin.com o la de sus empleados o agentes.
+				</p>
+
+				<h2>Indemnidad</h2>
+				<p>Usted se compromete a indemnizar y mantener www.Inkedin.com y sus empleados y agentes de y contra todas las responsabilidades, gastos legales, daños, pérdidas, costos y otros gastos en relación con cualquier reclamo o recursos interpuestos contra www.Inkedin.com que surja de cualquier incumplimiento por su parte de estos Términos y Condiciones o otras responsabilidades derivadas de su uso de este Sitio Web.</p>
+
+
+				<h2>Ruptura</h2>
+				<p>Si cualquiera de estos Términos y Condiciones debe ser considerado inválido, ilegal o inaplicable por cualquier razón, por cualquier tribunal de jurisdicción competente, dichos termonos y condiciones se cortarán  y los Términos y Condiciones restantes sobrevivirán y permanecerán en pleno vigor y efecto y seguirán siendo vinculantes y ejecutables.</p>
+
+
+				<h2>Ley vigente</h2>
+				<p>Estos Términos y Condiciones se regirán e interpretarán de conformidad con la ley de EE.UU. y que se someten a la jurisdicción exclusiva de los tribunales de Estados Unidos.</p>
+	
 		</div>
 	</section>
 	<!-- end content -->
